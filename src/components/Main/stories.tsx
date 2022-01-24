94e5a0b1f1f40b64dcad026727f670c497889092
Jhonata Martins da Costa <jhomcosta@gmail.com>
2022-01-23 21:15:14 -0300

    boilerplate feito em aula

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,15 @@
+import Main from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'Typescript, ReactJS, NextJS e Styled Components'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Default: Story = (args) => <Main {...args} />
